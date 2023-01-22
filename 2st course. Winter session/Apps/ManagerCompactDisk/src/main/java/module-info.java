37776477539be0b1b87c5e6d1a56459281140197
kulabuhov98 <kulabuhov98@mail.ru>
2023-01-23 02:08:16 +0300

    Update module-info.java

--- 2st course. Winter session/Apps/ManagerCompactDisk/src/main/java/module-info.java
+++ 2st course. Winter session/Apps/ManagerCompactDisk/src/main/java/module-info.java
@@ -1,13 +1,18 @@
-module ru.kulabuhov98 {
+module ru.main {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
+    requires java.sql;
+    requires mysql.connector.j;
 
-    opens ru.kulabuhov98 to javafx.fxml;
-    exports ru.kulabuhov98;
-    exports ru.kulabuhov98.controllers;
-    opens ru.kulabuhov98.controllers to javafx.fxml;
+    opens ru.main to javafx.fxml;
+    exports ru.main;
+    exports ru.main.controllers;
+    opens ru.main.controllers to javafx.fxml;
+
+    opens ru.main.models to javafx.base;
+    exports ru.main.models;
 }
