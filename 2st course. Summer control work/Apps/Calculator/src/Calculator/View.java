468742ecdd20bbf02b97e2ca4b67ac0dfa76eae5
kulabuhov98 <kulabuhov98@mail.ru>
2023-05-21 14:37:38 +0300

    Update View.java

--- 2st course. Summer control work/Apps/Calculator/src/Calculator/View.java
+++ 2st course. Summer control work/Apps/Calculator/src/Calculator/View.java
@@ -1,12 +1,12 @@
 package Calculator;
 
-import java.util.EmptyStackException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 public class View {
     public static void main(String[] args) {
         /* Экземляр класса Controller */
         Controller controller = new Controller();
+        /* Экземляр класса Model */
+        Model model = new Model();
         /* Экземляр класса Scanner */
         Scanner scanner = new Scanner(System.in);
         /* Выбранное действие в навигации */
@@ -17,7 +17,7 @@ public class View {
         try {
             /* Получение выбранного действия в навигации от пользователя */
             selectedAction = scanner.nextInt();
-        } catch (InputMismatchException e) {
+        } catch (Exception e) {
             /* Сообщение пользователю */
             System.out.println(e.getMessage());
         }
@@ -32,22 +32,27 @@ public class View {
                     String equation = scanner.next();
                     /* Передеча управления методу Calculator класса Controller */
                     System.out.print(controller.calculator(equation));
-                } catch (EmptyStackException | NumberFormatException e) {
+                } catch (Exception e) {
                     /* Сообщение пользователю */
                     System.out.println(e.getMessage());
+                } finally {
+                    /* Закрытие экземпляра класса Scanner */
+                    scanner.close();
                 }
                 break;
             case 2:
+                /* Получение истории уравнений */
+                model.showHistoryFromFile("equations.txt");
                 break;
             case 3:
+                /* Получение истории результатов работы калькулятора */
+                model.showHistoryFromFile("results.txt");
                 break;
             default:
                 /* Сообщение пользователю */
                 System.out.print("Exit...");
                 /* Выход из программы и завершение работы */
                 System.exit(1);
-                /* Закрытие экземпляра класса Scanner */
-                scanner.close();
         }
     }
 }
