c4f6956d6e5cf9ea286b4df1b3c47207e9d3d9d5
kulabuhov98 <kulabuhov98@mail.ru>
2023-05-21 11:45:13 +0300

    Create View.java

---
+++ 2st course. Summer control work/Apps/Calculator/src/Calculator/View.java
@@ -0,0 +1,55 @@
+package Calculator;
+
+import java.util.EmptyStackException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class View {
+    public static void main(String[] args) {
+        /* Экземляр класса Controller */
+        Controller controller = new Controller();
+        /* Экземляр класса Scanner */
+        Scanner scanner = new Scanner(System.in);
+        /* Выбранное действие в навигации */
+        int selectedAction = 0;
+
+        /* Сообщение пользователю */
+        System.out.println("Select an action: 1. Calculator 2. History of equations 3. History of results 4. Exit");
+        /* Исключение несоответствия входных данных */
+        try {
+            /* Получение выбранного действия в навигации от пользователя */
+            selectedAction = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            /* Сообщение пользователю */
+            System.out.println(e.getMessage());
+        }
+
+        /* Обработка выбранного действия в навигации */
+        switch (selectedAction) {
+            case 1:
+                /* Сообщение пользователю */
+                System.out.print("Enter the equation: ");
+                /* Исключения несоответствия входных данных */
+                try {
+                    /* Получение уравнения от пользователя */
+                    String equation = scanner.next();
+                    /* Передеча управления методу Calculator класса Controller */
+                    System.out.print(controller.calculator(equation));
+                } catch (EmptyStackException | NumberFormatException e) {
+                    /* Сообщение пользователю */
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                /* Сообщение пользователю */
+                System.out.print("Exit...");
+                /* Выход из программы и завершение работы */
+                System.exit(1);
+                /* Закрытие экземпляра класса Scanner */
+                scanner.close();
+        }
+    }
+}
