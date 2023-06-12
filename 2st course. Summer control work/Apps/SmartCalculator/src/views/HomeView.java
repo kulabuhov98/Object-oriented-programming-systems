c98e36f0ca3b77eb6e6ab2ffb03db814c35495bc
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-12 15:35:23 +0300

    Update HomeView.java

--- 2st course. Summer control work/Apps/SmartCalculator/src/views/HomeView.java
+++ 2st course. Summer control work/Apps/SmartCalculator/src/views/HomeView.java
@@ -19,8 +19,24 @@ public class HomeView {
     /* Основной экран приложения */
     public void main() {
         try {
-            
+            /* Сообщение пользователю */
+            System.out.println("Navigation: \n1. Calculator \n2. Show expressions and results \n3. Save results \n4. Exit");
+            /* Обработка выбранного действия в навигации */
+            switch (scanner.nextInt()) {
+                case 1 -> {
+
+                }
+                case 2 -> {
+
+                }
+                case 3 -> {
+
+                }
+                /* Выход и завершение работы приложения */
+                default -> homeController.exit();
+            }
         } catch (Exception e) {
+            /* Сообщение пользователю */
             System.out.println(e.getMessage());
         } finally {
             scanner.close();
