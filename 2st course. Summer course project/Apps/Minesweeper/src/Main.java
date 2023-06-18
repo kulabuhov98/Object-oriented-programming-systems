9b22b28a50e6849698d4e987b7838650f1d6a21c
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-18 18:16:06 +0300

    Update Main.java

--- 2st course. Summer course project/Apps/Minesweeper/src/Main.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/Main.java
@@ -1,15 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class Main extends JFrame {
-    /* Элемент управления, представляющий собой прямоугольное пространство */
-    private JPanel jPanel;
     /* Количество столбцов на игровом поле по умолчанию */
-    private final int COLS = 16;
+    private final int COLS = 15;
     /* Количество строк на игровой поле по умолчанию */
-    private final int ROWS = 16;
+    private final int ROWS = 1;
     /* Размер изображения на игровом поле */
-    private final int IMAGE_SIZE = 50;
+    private final int IMAGE_SIZE = 16;
     public static void main(String[] args) {
         /* Экземляр класса Main */
         new Main();
@@ -41,11 +40,21 @@ public class Main extends JFrame {
 
     /* Инициализация JPanel */
     private void initJPanel() {
-        /* Экземляр класса JPanel */
-        jPanel = new JPanel();
+        /* Элемент управления, представляющий собой прямоугольное пространство */
+        JPanel jPanel = new JPanel();
         /* Установка предпочтительного размера компонента JPanel */
-        jPanel.setPreferredSize(new Dimension(500, 500));
+        jPanel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));
         /* Добавление компонента JPanel в окно приложения */
         add(jPanel);
+    }
+
+    /* Получение изображения для игрового поля */
+    private Image getImage(String filename) {
+        /* Формирование пути до изображения */
+        String resource = "img/" + filename.toLowerCase() + ".png";
+        /* Получение изображения из ресурсов проекта */
+        ImageIcon imageIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource(resource)));
+        /* Возвращение изображения для игрового поля */
+        return imageIcon.getImage();
     }
 }
