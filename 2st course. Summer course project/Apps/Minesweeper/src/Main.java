bba2d8777d5f6bc168ae8a74a31fe5aba7fe53b1
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-18 16:10:49 +0300

    Update Main.java

--- 2st course. Summer course project/Apps/Minesweeper/src/Main.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/Main.java
@@ -1,11 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
-    /*
-    *
-    *
-    *
-    */
+    /* Элемент управления, представляющий собой прямоугольное пространство */
+    private JPanel jPanel;
     public static void main(String[] args) {
         /* Экземляр класса Main */
         new Main();
@@ -13,11 +11,13 @@ public class Main extends JFrame {
 
     /* Конструктор класса Main */
     private Main() {
+        /* Вызов метода initJPanel класса Main */
+        initJPanel();
         /* Вызов метода initJFrame класса Main */
         initJFrame();
     }
 
-    /* Инициализация приложения */
+    /* Инициализация JFrame */
     private void initJFrame() {
         /* Действие при завершении приложения */
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -31,5 +31,15 @@ public class Main extends JFrame {
         pack();
         /* Отображение окна приложения по центру экрана */
         setLocationRelativeTo(null);
+    }
+
+    /* Инициализация JPanel */
+    private void initJPanel() {
+        /* Экземляр класса JPanel */
+        jPanel = new JPanel();
+        /* Установка предпочтительного размера компонента JPanel */
+        jPanel.setPreferredSize(new Dimension(500, 500));
+        /* Добавление компонента JPanel в окно приложения */
+        add(jPanel);
     }
 }
