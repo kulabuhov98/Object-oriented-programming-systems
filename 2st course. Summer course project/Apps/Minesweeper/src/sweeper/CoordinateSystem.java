eba0bb9dd6e8f01ef85a639692090687d3a3f7ac
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-20 22:19:03 +0300

    Update CoordinateSystem.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/CoordinateSystem.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/CoordinateSystem.java
@@ -7,7 +7,7 @@ public class CoordinateSystem {
     /* Значение координаты по оси Y */
     public int y;
 
-    /*  */
+    /* Конструктор класса CoordinateSystem */
     public CoordinateSystem(int x, int y) {
         this.x = x;
         this.y = y;
