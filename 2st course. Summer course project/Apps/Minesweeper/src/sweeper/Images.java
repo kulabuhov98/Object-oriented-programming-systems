b1b23ca294dd18ad438422d21d9f6178ee31e488
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-20 21:58:56 +0300

    Create Images.java

---
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Images.java
@@ -0,0 +1,25 @@
+package sweeper;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+public class Images {
+    /* Установка взаимосвязи между изображениями из ресурсов проекта с элементами перечисления */
+    public void setImages() {
+        /* Прохождение по всем элементам перечисления */
+        for (ImagesForCell imagesForCell : ImagesForCell.values()) {
+            /* Установка взаимосвязи между изображением с элементом перечисления */
+            imagesForCell.image = getImage((imagesForCell.name()));
+        }
+    }
+
+    /* Получение изображения из ресурсов проекта  */
+    public Image getImage(String filename) {
+        /* Формирование пути до изображения */
+        String resource = "../" + filename.toLowerCase() + ".png";
+        /* Получение изображения из ресурсов проекта */
+        ImageIcon imageIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource(resource)));
+        /* Возвращение изображения для ячейки игрового поля */
+        return imageIcon.getImage();
+    }
+}
