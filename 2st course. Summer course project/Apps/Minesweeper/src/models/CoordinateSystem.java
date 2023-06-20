55c94984c0e23f182200c426e37befe6d3ee11ed
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-20 21:22:55 +0300

    Create CoordinateSystem.java

---
+++ 2st course. Summer course project/Apps/Minesweeper/src/models/CoordinateSystem.java
@@ -0,0 +1,15 @@
+package models;
+
+public class CoordinateSystem {
+    /* Значение координаты по оси X */
+    public int x;
+
+    /* Значение координаты по оси Y */
+    public int y;
+
+    /* Конструктор класса CoordinateSystem */
+    public CoordinateSystem(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
