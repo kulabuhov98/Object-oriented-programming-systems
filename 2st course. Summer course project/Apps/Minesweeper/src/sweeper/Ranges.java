1539f71863847427353315d8d94fa7e3647780cb
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-21 21:48:20 +0300

    Update Ranges.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Ranges.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Ranges.java
@@ -48,4 +48,23 @@ public class Ranges {
     static CoordinateSystem getRandomCoordinate() {
         return new CoordinateSystem(random.nextInt(size.x), random.nextInt(size.y));
     }
+
+    /* Получение всех координат оси X и Y вокруг указанной координаты */
+    static ArrayList<CoordinateSystem> getCoordinatesAround(CoordinateSystem coordinateSystem) {
+        /* Исходная координата оси X и Y */
+        CoordinateSystem centerAround;
+        /* Координаты оси X и Y вокруг указанной координаты */
+        ArrayList<CoordinateSystem> coordinateSystemArrayList = new ArrayList<>();
+        /* Прохождение по координатам оси Y */
+        for (int y = coordinateSystem.y - 1; y <= coordinateSystem.y + 1; y++)
+            /* Прохождение по координатам оси X */
+            for (int x = coordinateSystem.x - 1; x <= coordinateSystem.x + 1; x++)
+                /* Находится ли указанная координата на игровом поле */
+                if (inRangePlayingField(centerAround = new CoordinateSystem(x, y)))
+                    /* Сравнение исходной координаты с указанной координатой */
+                    if (!centerAround.equals(coordinateSystem))
+                        /* Формирование списках всех координат оси X и Y вокруг указанной координаты */
+                        coordinateSystemArrayList.add(centerAround);
+        return coordinateSystemArrayList;
+    }
 }
