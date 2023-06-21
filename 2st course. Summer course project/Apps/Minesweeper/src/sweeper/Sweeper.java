0f6f935f5ae5b003ee1dce49cc74fbad5dbcf6f7
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-21 19:46:41 +0300

    Delete Sweeper.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Sweeper.java
+++
@@ -1,9 +0,0 @@
-package sweeper;
-
-public class Sweeper {
-    /* Конструктор класса Sweeper */
-    public Sweeper (int cols, int rows) {
-        Ranges.setSize(new CoordinateSystem(cols, rows));
-    }
-
-}
