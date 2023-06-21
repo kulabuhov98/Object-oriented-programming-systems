f5848e97a67ad8f4523eb67090fcd57488960c46
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-21 22:43:48 +0300

    Update Game.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Game.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Game.java
@@ -1,24 +1,33 @@
 package sweeper;
 
 public class Game {
-    /* Карта для объектов нижнего слоя */
-    private MapLayerBottom mapLayerBottom;
     /* Карта для объектов верхнего слоя */
     private MapLayerTop mapLayerTop;
+
+    /* Карта для объектов нижнего слоя */
+    private MapLayerBottom mapLayerBottom;
 
     /* Конструктор класса Game */
     public Game(int cols, int rows, int bombs) {
         Ranges.setSize(new CoordinateSystem(cols, rows));
+        mapLayerTop = new MapLayerTop();
         mapLayerBottom = new MapLayerBottom(bombs);
     }
 
     /* Получение изображения из ячейки игрового поля */
     public ImagesEnum getImageFromCell(CoordinateSystem coordinateSystem) {
-        return mapLayerBottom.getImageEnum(coordinateSystem);
+        /* Ячейка на игровом поле открыта */
+        if (mapLayerTop.getImageEnum(coordinateSystem) == ImagesEnum.OPENED)
+            /* Взаимодействие с картой для объектов нижнего слоя */
+            return mapLayerBottom.getImageEnum(coordinateSystem);
+        else
+            /* Взаимодействие с картой для объектов верхнего слоя */
+            return mapLayerTop.getImageEnum(coordinateSystem);
     }
 
     /* Инициализация новой игры */
     public void initNewGame() {
+        mapLayerTop.initMapLayerTop();
         mapLayerBottom.initMapLayerBottom();
     }
 }
