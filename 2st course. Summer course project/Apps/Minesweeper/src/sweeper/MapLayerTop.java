0b12c0767a075845e33f3fb4d36236569c61ee28
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-21 22:43:50 +0300

    Update MapLayerTop.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/MapLayerTop.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/MapLayerTop.java
@@ -1,6 +1,17 @@
 package sweeper;
 
-public class MapLayerTop {
+class MapLayerTop {
     /* Карта для объектов верхнего слоя */
     private MapMatrix mapLayerTop;
+
+    /* Инициализация карты для объектов верхнего слоя */
+    void initMapLayerTop() {
+        /* Объект класса MapMatrix */
+        mapLayerTop = new MapMatrix(ImagesEnum.CLOSED);
+    }
+
+    /* Получение элемента перечисления из карты для объектов верхнего слоя с указаннами координатами */
+    ImagesEnum getImageEnum(CoordinateSystem coordinateSystem) {
+        return mapLayerTop.getImageEnum(coordinateSystem);
+    }
 }
