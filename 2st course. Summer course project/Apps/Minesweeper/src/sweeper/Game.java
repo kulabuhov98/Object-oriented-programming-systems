11ef809189edb3ec3a8d62cb1a9ea4dda4ae968e
kulabuhov98 <kulabuhov98@mail.ru>
2023-06-22 13:03:24 +0300

    Update Game.java

--- 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Game.java
+++ 2st course. Summer course project/Apps/Minesweeper/src/sweeper/Game.java
@@ -7,7 +7,7 @@ public class Game {
     /* Карта для объектов нижнего слоя */
     private MapLayerBottom mapLayerBottom;
 
-    /* Текущее состояние игры */
+    /* Перечисление с состояниями игры */
     private GameStateEnum gameState;
 
     /* Конструктор класса Game */
@@ -17,9 +17,14 @@ public class Game {
         mapLayerBottom = new MapLayerBottom(bombs);
     }
 
-    /* Получение текущего состояния игры */
-    public GameStateEnum getGameState() {
-        return gameState;
+    /* Инициализация новой игры */
+    public void initNewGame() {
+        /* Инициализация карты для объектов верхнего слоя */
+        mapLayerTop.initMapLayerTop();
+        /* Инициализация карты для объектов нижнего слоя */
+        mapLayerBottom.initMapLayerBottom();
+        /* Установка состояние игры - в процессе игры */
+        gameState = GameStateEnum.PLAYED;
     }
 
     /* Получение изображения из ячейки игрового поля */
@@ -33,19 +38,46 @@ public class Game {
             return mapLayerTop.getImageEnum(coordinateSystem);
     }
 
-    /* Инициализация новой игры */
-    public void initNewGame() {
-        /* Инициализация карты для объектов верхнего слоя */
-        mapLayerTop.initMapLayerTop();
-        /* Инициализация карты для объектов нижнего слоя */
-        mapLayerBottom.initMapLayerBottom();
-        /* Установка состояние игры - в процессе игры */
-        gameState = GameStateEnum.PLAYED;
-    }
-
     /* Обработка нажатия левой кнопки мыши */
     public void pressLeftMouseButton(CoordinateSystem coordinateSystem) {
+        cellBehavior(coordinateSystem);
+    }
+
+    /* Обработка поведения ячейки верхнего слоя игрового поля */
+    private void cellBehavior(CoordinateSystem coordinateSystem) {
+        /* Получение текущего элемента перечисления в указанной координате на верхнем слое игрового поля */
+        switch (mapLayerTop.getImageEnum(coordinateSystem)) {
+            /* Является ли указанная ячейка открытой */
+            case OPENED: return;
+            /* Находится ли в указанной координате флаг */
+            case FLAGED: return;
+            /* Является ли указанная ячейка закрытой */
+            case CLOSED:
+                /* Получение текущего элемента перечисления в указанной координате на нижнем слое игрового поля */
+                switch (mapLayerBottom.getImageEnum(coordinateSystem)) {
+                    /* Является ли указанная ячейка пустой */
+                    case ZERO: setOpenedCellAround(coordinateSystem); return;
+                    /* Находится ли в указанной координате бомба */
+                    case BOMB: return;
+                    /* Находятся ли в указанной координате цифры 1 - 8 */
+                    default: mapLayerTop.setOpenedCell(coordinateSystem); return;
+                }
+        }
+    }
+
+    /* Установка элемента перечисления OPENED вокруг указанной координаты */
+    private void setOpenedCellAround(CoordinateSystem coordinateSystem) {
+        /* Вызов метода setOpenedCell класса MapLayerTop */
         mapLayerTop.setOpenedCell(coordinateSystem);
+        /* Прохождение по списку всех координат оси X и Y вокруг указанной координаты */
+        for (CoordinateSystem around : Ranges.getCoordinatesAround(coordinateSystem))
+            /* Вызов метода cellBehavior класса Game */
+            cellBehavior(around);
+    }
+
+    /* Определение победы в игре */
+    private void checkGameWinner() {
+        
     }
 
     /* Обработка нажатия правой кнопки мыши */
